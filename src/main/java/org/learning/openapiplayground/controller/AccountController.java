3caacb7c1618e4f741413202438c915f4416cc60
Eddie Leung <eddie.leung@foxtel.com.au>
2023-06-01 18:52:21 +1000

    Initial commit

---
+++ src/main/java/org/learning/openapiplayground/controller/AccountController.java
@@ -0,0 +1,2 @@
+package org.learning.openapiplayground.controller;public class AccountController {
+}
---
+++ src/test/java/org/learning/openapiplayground/OpenApiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.openapiplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
