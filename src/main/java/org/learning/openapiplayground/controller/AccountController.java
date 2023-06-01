c22878af64341e218e84adc95b11eb651b7aa7ba
Eddie Leung <eddie.leung@foxtel.com.au>
2023-06-02 09:14:41 +1000

    Modified AccountController's default implementation

--- src/main/java/org/learning/openapiplayground/controller/AccountController.java
+++ src/main/java/org/learning/openapiplayground/controller/AccountController.java
@@ -4,7 +4,6 @@ import org.openapitools.api.AccountApi;
 import org.openapitools.model.Account;
 import org.openapitools.model.DepositRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,12 +13,16 @@ public class AccountController implements AccountApi {
     @Override
     public ResponseEntity<Void> depositToAccount(DepositRequest depositRequest) {
         //Execute the business logic through Service/Utils/Repository classes
-        return AccountApi.super.depositToAccount(depositRequest);
+        //return AccountApi.super.depositToAccount(depositRequest);
+
+        return ResponseEntity.ok().build();
     }
 
     @Override
     public ResponseEntity<Account> getAccount() {
         //Execute the business logic through Service/Utils/Repository classes
-        return AccountApi.super.getAccount();
+        //return AccountApi.super.getAccount();
+
+        return ResponseEntity.ok(new Account());
     }
 }
