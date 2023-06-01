49f85cd1f686fa1d3b31aaed2e8fef70bca44826
Eddie Leung <eddie.leung@foxtel.com.au>
2023-06-01 18:52:35 +1000

    OpenAPI exercise for https://www.baeldung.com/spring-boot-openapi-api-first-development

--- src/main/java/org/learning/openapiplayground/controller/AccountController.java
+++ src/main/java/org/learning/openapiplayground/controller/AccountController.java
@@ -1,2 +1,25 @@
-package org.learning.openapiplayground.controller;public class AccountController {
+package org.learning.openapiplayground.controller;
+
+import org.openapitools.api.AccountApi;
+import org.openapitools.model.Account;
+import org.openapitools.model.DepositRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping
+public class AccountController implements AccountApi {
+
+    @Override
+    public ResponseEntity<Void> depositToAccount(DepositRequest depositRequest) {
+        //Execute the business logic through Service/Utils/Repository classes
+        return AccountApi.super.depositToAccount(depositRequest);
+    }
+
+    @Override
+    public ResponseEntity<Account> getAccount() {
+        //Execute the business logic through Service/Utils/Repository classes
+        return AccountApi.super.getAccount();
+    }
 }
--- src/test/java/org/learning/openapiplayground/OpenApiPlaygroundApplicationTests.java
+++ src/test/java/org/learning/openapiplayground/OpenApiPlaygroundApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OpenApiPlaygroundApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
